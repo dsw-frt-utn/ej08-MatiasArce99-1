af866cb6667d824e82c1b0b63ca93ae847c65def
Matías <matiasarce214@gmail.com>
2026-05-04 10:05:23 -0300

    Primera versión problema 6

---
+++ Dsw2026Ej8/Problema6/Problema6.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema6;
+
+public class Problema6
+{
+    public string NormalizarCodigoProducto(string code)
+    {
+        return $"{code}";
+    }
+}
