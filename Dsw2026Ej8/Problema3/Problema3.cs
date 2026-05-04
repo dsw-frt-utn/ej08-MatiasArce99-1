731d21fd8e27c1c7c6e4b93242f22225da4d02a9
Matías <matiasarce214@gmail.com>
2026-05-04 08:28:57 -0300

    Solución al problema 3

---
+++ Dsw2026Ej8/Problema3/Problema3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema3;
+
+public class Problema3
+{
+    public string CompararCopias(int originalValue, Product product)
+    {
+        int _variableLocal = originalValue;
+        _variableLocal++;
+
+        Product p = product;
+        p.SetDescripcion("Producto modificado");
+
+        return $"{originalValue} - {_variableLocal} - {p.GetDescripcion()}";
+    }
+}
