380bb3bd0b3978e613e65ba5d20a588a20c675b4
Matías <matiasarce214@gmail.com>
2026-05-04 08:19:05 -0300

    Solución al problema 2

---
+++ Dsw2026Ej8/Problema2/Problema2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema2;
+
+public class Problema2
+{
+    public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+    {
+        double total;
+
+        if (quantity <= 0)
+        {
+            total = 0;
+        } else
+        {
+            total = (double)(quantity * unitPrice);
+        }
+
+        var resumen = new
+        {
+            code = productCode,
+            description = productDescription,
+            quantity = quantity,
+            total = total
+        };
+
+        return $"{resumen.code} - {resumen.description} - {resumen.total}";
+    }
+}
