85315848d88e7fb5cdf82bc91fb5b7315564fd15
Matías <matiasarce214@gmail.com>
2026-05-04 08:07:16 -0300

    Solución al problema 1

---
+++ Dsw2026Ej8/Problema1/SegundoArchivo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema1;
+
+public partial class ProductHelper
+{
+    public string FormatearPrecio(decimal price)
+    {
+        return price.ToString("0.00");
+    }
+}
