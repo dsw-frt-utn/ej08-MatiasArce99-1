d1fc23bbb4377f7001e19eb8429ae086290dfb88
Matías <matiasarce214@gmail.com>
2026-05-04 09:38:44 -0300

    Primera versión del problema 4

---
+++ Dsw2026Ej8/Problema4/Problema4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema4;
+
+public class Problema4
+{
+    public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+    {
+        double promedio;
+
+        if (nota1 != null && nota2 != null && nota3 != null)
+        {
+            promedio = (double)(nota1 + nota2 + nota3)/3;
+        } else
+        {
+            Console.WriteLine("Las notas no pueden ser NULL");
+        }
+        return $"{promedio}";
+    }
+}
