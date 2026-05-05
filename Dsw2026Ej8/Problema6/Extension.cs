ed79d5d55f0d15b4842d568579f789b392295167
Matías <matiasarce214@gmail.com>
2026-05-04 21:40:02 -0300

    Solución del problema 6

---
+++ Dsw2026Ej8/Problema6/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema6;
+
+public static class Extension
+{
+    public static string ToProductCode(this string code)
+    {
+        if (code == null)
+        {
+            return "SIN-CODIGO";
+        }
+        return code.Trim().ToUpper().Replace(" ", "-");
+    }
+}
--- Dsw2026Ej8/Problema6/Problema6.cs
+++ Dsw2026Ej8/Problema6/Problema6.cs
@@ -8,6 +8,6 @@ public class Problema6
 {
     public string NormalizarCodigoProducto(string code)
     {
-        return $"{code}";
+        return code.ToProductCode();
     }
 }
